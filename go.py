2f8725eb03e8309761972db718c016adedb9e5dc
miceuz <mic@wemakethings.net>
2014-06-11 00:17:01 +0200

    shuffle around the code

--- go.py
+++ go.py
@@ -6,14 +6,25 @@ import time
 import fastopc as opc
 from core import NoiseGenerator
 
+class NoiseParams:
+    octaves = 1
+    persistence = 0.5
+    lacunarity = 2.0
+    def __init__(self, octaves, persistence, lacunarity):
+        self.octaves = octaves
+        self.persistence = persistence
+        self.lacunarity = lacunarity
+        
+mainNoiseParams = NoiseParams(5, 0.7, 2.0)
+
 targetFPS = 24
 targetFrameTime = 1./targetFPS
-screen = Screen(['127.0.0.1:7891'])
+screen = Screen(sections, ['127.0.0.1:7891'])
 
 timeCounter = 0
 print("eina.. Control+C to stop")
 while True:
-    bitmap = NoiseGenerator.get2dNoise(32,32, timeCounter/20.0, 5,0.7,2)
+    bitmap = NoiseGenerator.get2dNoise(32,32, timeCounter/20.0, mainNoiseParams.octaves, mainNoiseParams.persistence, mainNoiseParams.lacunarity)
 
     startTime = time.time()
 
--- screen.py
+++ screen.py
@@ -15,18 +15,18 @@ def toRGBBytes(value):
     return [(value >> 16) & 0x0000FF, (value >> 8) & 0x0000FF, value & 0x0000FF]
 
 class Screen():
-    leds = opc.FastOPC()
-    auxscreens = []
+    opcServers = []
     pixelMap = []
     
-    
-    def __init__(self, auxscreens = []):
-        if None != auxscreens:
-            for aux in auxscreens:
-                self.auxscreens.append(opc.FastOPC(aux))
-        for section in sections:
+    def __init__(self, ledStrandModel, auxscreens = []):
+        for section in ledStrandModel:
             self.pixelMap += self.createMapFor(section)
         self.pixelMapPacked = np.array(self.pixelMap).astype(np.int8).tostring()
+
+        self.opcServers.append(opc.FastOPC())
+        if None != auxscreens:
+            for aux in auxscreens:
+                self.opcServers.append(opc.FastOPC(aux))
             
     def createMapFor(self, section):
         pixmap = []
@@ -36,20 +36,12 @@ class Screen():
             x = x + led['xstep']
             y = y + led['ystep']
             pixmap.append((x, y))
-        section['map'] = pixmap
         return pixmap
         
-    def getPixelsFor(self, section, bitmap):
-        pixels = [None] * len(section['pattern'])
-        for i in range(len(section['pattern'])):
-            y = section['map'][i][1]
-            x = section['map'][i][0]
-            pixels[i] = toRGBBytes(bitmap[y][x])
-        return pixels
-
     def send(self, bitmap):
         bitmapPacked = struct.pack('I'*len(bitmap)*len(bitmap[0]), *(j for i in bitmap for j in i)) 
-	tosend = core.PixelMapper.map(self.pixelMapPacked, bitmapPacked)
-        self.leds.putPixels(0, tosend)
-        for aux in self.auxscreens:
-            aux.putPixels(0, tosend)
+
+        tosend = core.PixelMapper.map(self.pixelMapPacked, bitmapPacked)
+
+        for out in self.opcServers:
+            out.putPixels(0, tosend)
