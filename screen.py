f68ef1372f1ddf54877e681fa99ddac821676571
miceuz <mic@hardcore.lt>
2014-06-15 11:15:59 -0600

    run faster, pack numpy array by actually using numpy

--- screen.py
+++ screen.py
@@ -44,7 +44,7 @@ class Screen():
         return pixmap
         
     def send(self, bitmap):
-        bitmapPacked = struct.pack('I'*len(bitmap)*len(bitmap[0]), *(j for i in bitmap for j in i)) 
+        bitmapPacked = bitmap.astype(np.uint32).tostring()#struct.pack('I'*len(bitmap)*len(bitmap[0]), *(j for i in bitmap for j in i)) 
         tosend = core.PixelMapper.map(self.pixelMapPacked, bitmapPacked, len(bitmap[0]), len(bitmap))
         for out in self.opcServers:
             out.putPixels(0, tosend)
